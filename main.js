c4b462199a35f40d7e2a7ad0d89539040ec49b07
srinithyaNalluru <srinithyanalluru29@gmail.com>
2022-10-01 16:48:18 +0530

    layout styling, added button functions

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
-// var i11 = document.getElementById("11").value;
-
-// console.log(i11);
 function getVal() {
-    const val11 = document.getElementById("11").value;
-    const val12 = document.getElementById("12").value;
-    const val13 = document.getElementById("13").value;
-    const val21 = document.getElementById("21").value;
-    const val22 = document.getElementById("22").value;
-    const val23 = document.getElementById("23").value;
-    const val31 = document.getElementById("31").value;
-    const val32 = document.getElementById("32").value;
-    const val33 = document.getElementById("33").value;
-    console.table([val11, val12, val13], [val21, val22, val23],[val31, val32, val33]);
+    let arr=[];
+    for(var i=1;i<=9;i++){
+        let temp=[];
+        for(var j=1;j<=9;j++){
+            var s=i.toString() + j.toString();
+            var x=document.getElementById(s).value;
+            temp.push(parseInt(x))
+        }
+        arr.push(temp);
+    }
+    console.table(arr);
 }
 
+function clearAll() {
+    for(var i=1;i<=9;i++){
+        for(var j=1;j<=9;j++){
+            var s=i.toString() + j.toString();
+            document.getElementById(s).value="";
+        }
+        console.log("row"+i+"cleared")
+    }
+    console.log("all cleared")
+}
+
+function fillRandom() {
+    var arr=[[5,3,0,0,7,0,0,0,0],
+            [6,0,0,1,9,5,0,0,0],
+            [0,9,8,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9]]
+    for(var i=1;i<=9;i++){
+        for(var j=1;j<=9;j++){
+            var s=i.toString() + j.toString();
+            document.getElementById(s).value=arr[i-1][j-1];
+        }
+    }
+}
+
+
 document.getElementById("get").onclick = getVal;
+document.getElementById("clear").onclick = clearAll;
+document.getElementById("fill").onclick = fillRandom;
